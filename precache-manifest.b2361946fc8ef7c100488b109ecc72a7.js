3e887abda0a23aa3be1b70552ecadc5d45a72130
AlsayeghN <nouralsayegh9@gmail.com>
2021-12-17 10:58:37 +0200

    Updates

---
+++ precache-manifest.b2361946fc8ef7c100488b109ecc72a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b777edc43c198f85014cf5c2eff975d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "23af2218c86805aebb4c",
+    "url": "/react-deploy/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "23af2218c86805aebb4c",
+    "url": "/react-deploy/static/js/2.141b0d76.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/react-deploy/static/js/2.141b0d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9fb520d603f5c25c37",
+    "url": "/react-deploy/static/js/main.87cb5bc1.chunk.js"
+  },
+  {
+    "revision": "c4abad42caa738f42bbc",
+    "url": "/react-deploy/static/js/runtime-main.ad965e23.js"
+  }
+]);
